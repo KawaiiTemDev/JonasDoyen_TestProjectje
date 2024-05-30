f204cb263f9ea4c004daeb35e12c758430ad2b08
aleksandr.polovyna <aleksandr.polovyna@SJO-PC017>
2024-05-30 14:19:24 +0200

    knopkleur veranderd (pull)

--- JonasDoyenTestProjectje/Form1.Designer.cs
+++ JonasDoyenTestProjectje/Form1.Designer.cs
@@ -44,12 +44,13 @@ namespace JonasDoyenTestProjectje
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.Sienna;
             this.button1.Location = new System.Drawing.Point(266, 123);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(244, 127);
             this.button1.TabIndex = 1;
             this.button1.Text = "PODLOOOODDD";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
